b05d1c938577cb0e419ea85722a24b681b1b05f6
EvandroArmando <53578532+EvandroArmando@users.noreply.github.com>
2022-06-01 17:13:01 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 const express =require('express');
+const { json } = require('express/lib/response');
 
 const app =express();
-
+app.use(express.json())
 
 app.get('/',(req, response) =>
 {
     return response.json({Message:"Hello word Ignite"});
 }
-)
+
+
+);
+
+//req.params =serve para editar os campos da rota
+//req.query= request.query = paginação filtro
+//req.body= é para pegar os dados 
+//metodo post
+
+app.post('/courses',(req, res)=>{
+
+    
+
+    return response.json(['Curso 1','Curso 02','Curso3'])
+
+} )
+
+//quatro metodos
+
+
 
 app.listen(3333);

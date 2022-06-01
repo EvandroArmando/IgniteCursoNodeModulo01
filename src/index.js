c3ec57f516aff0428fd1f33c86371ab18e7bcce0
EvandroArmando <53578532+EvandroArmando@users.noreply.github.com>
2022-06-01 20:03:14 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,59 @@
 const express =require('express');
 const { json } = require('express/lib/response');
-
+const { v4: uuidv4 } = require('uuid') //coloca o v4 para gerar número randomicos
 const app =express();
 app.use(express.json())
-
-app.get('/',(req, response) =>
-{
-    return response.json({Message:"Hello word Ignite"});
-}
-
-
-);
 
 //req.params =serve para editar os campos da rota
 //req.query= request.query = paginação filtro
 //req.body= é para pegar os dados 
 //metodo post
-
-app.post('/courses',(req, res)=>{
-
-    
-
-    return response.json(['Curso 1','Curso 02','Curso3'])
-
-} )
-
 //quatro metodos
+//ver funcionamento API
+
+
+//declaraçao das variaveis
+
+
+
+const customers= [];
+
+app.get('/',(req, response) =>
+{
+    return response.json({Message:"Hello word Ignite"});
+});
+
+
+ 
+
+
+
+//Cadastro de conta
+app.post('/account',(req,response)=>{
+
+    const{cpf,name} =req.body;
+    const id= uuidv4();//gerar valores aleartorios
+
+//salvar elementos em um array
+customers.push(
+    {cpf,
+    name,
+    id,
+    statement:[]
+
+}
+
+
+)
+console.log('objecto salvo',customers)
+
+
+ 
+
+return response.status(200).send();
+
+})
+  
 
 
 
